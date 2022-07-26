7b33e47c2647e0121fbe8841199cdde167c57145
Stelmakh Bohdan <b.stelmakh@bsg.world>
2022-07-26 06:21:59 +0300

    add todos app

---
+++ src/components/TodoItem.svelte
@@ -0,0 +1,26 @@
+<script>
+  export let message;
+  export let index;
+  export let removeHandler;
+</script>
+
+<li class="item">
+  <span>Number {index + 1}: {message}</span>
+  <button on:click={() => removeHandler(index)} class="remove_btn">Remove</button>
+</li>
+
+<style>
+  .item {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    margin: 5px 0;
+  }
+
+  .remove_btn {
+    background-color: orangered;
+    padding: 2px;
+    color: #ffffff;
+    font-size: 14px;
+  }
+</style>
---
+++ src/components/TodoList.svelte
@@ -0,0 +1,69 @@
+<script>
+  import TodoItem from './TodoItem.svelte';
+
+  let newTodo = '';
+  let todos = [];
+
+  const addTodo = () => {
+    if (newTodo) {
+      todos = [...todos, newTodo];
+      newTodo = '';
+    }
+  }
+
+  const removeHandler = (id) => {
+    todos = [...todos.filter((item, index) => index !== id)]
+  }
+
+</script>
+
+<div>
+  <div class="tl">
+    <label for="input_msg">Todo name</label>
+    <input bind:value={newTodo} id="input_msg" type="text" placeholder="Text your message..." class="input_msg"/>
+    <button class="add_task_btn" on:click={addTodo}>Add new task</button>
+  </div>
+
+
+
+
+  <ul class="list">
+    {#each todos as message, i}
+      <TodoItem message={message} index={i} removeHandler={removeHandler} />
+    {/each}
+  </ul>
+</div>
+
+
+<style>
+  .tl {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    width: 400px;
+    margin: 0 auto;
+  }
+
+  .add_task_btn {
+    padding: 10px;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    color: #ffffff;
+    background-color: #213547;
+  }
+
+  .list {
+    padding: 5px;
+    margin-left: 51px;
+    border: 1px solid mediumpurple;
+
+  }
+
+  .input_msg, .input_msg:focus-visible {
+    padding: 5px;
+    border-radius: 5px;
+    border: none;
+    outline: none;
+  }
+</style>
